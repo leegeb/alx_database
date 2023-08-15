a1ceed24db3b1cb3e0a35ac3f348f9353b73a0d1
lydia gebre <lydiakgebre@gmail.com>
2023-08-15 06:31:47 +0400

    Inserting a value

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- file: 7-insert_value.sql
+-- Description: Below script that inserts a new row in the table first_table (database hbtn_0c_0) in my MySQL server.
+SELECT id, name FROM hbtn_0c_0.first_table;
