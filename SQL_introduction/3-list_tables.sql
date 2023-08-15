f8f82d73f34a682a8287a865b765d9ce42b47261
lydia gebre <lydiakgebre@gmail.com>
2023-08-15 05:25:54 +0400

    Update 3-list_tables.sql

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -2,5 +2,5 @@
 -- Description: script that lists all the tables of a database in my MySQL server.
 
 -- The database name will be passed as argument of mysql command
-mysql -h localhost -u root -p -e "USE mysql; SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = DATABASE();"
-"
+mysql -h localhost -u root -p -e "USE mysql; SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'mysql';"
+
