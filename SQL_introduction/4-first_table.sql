f5446b3790b5e920f3b715f5703f8fcf5348af66
lydia gebre <lydiakgebre@gmail.com>
2023-08-15 05:34:08 +0400

    creating tables

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- file: 4-first_table.sql
+-- Description: Below script creates a table called first_table in the current database in my MySQL server
+
+mysql -h localhost -u root -p -N -e "USE hbtn_0c_0; SHOW TABLES;"
