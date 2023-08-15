3ad447c5ec150ae52fa96f6c63ba64fab220d649
lydia gebre <lydiakgebre@gmail.com>
2023-08-15 04:43:13 +0400

    Lists-MySQL-databases

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -3,9 +3,6 @@
 
 -- This script lists all databases of my MySQL server.
 
--- Display a message to indicate the beginning of the script.
-SELECT 'Enter password:' AS Message;
-
 -- Use the SHOW DATABASES statement to list databases.
 -- The SHOW DATABASES statement returns a list of all databases on the server.
 SHOW DATABASES;
