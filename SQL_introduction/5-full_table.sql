9996500a884ae0a70783e8b61e789b68908a0b29
lydia gebre <lydiakgebre@gmail.com>
2023-08-15 06:18:07 +0400

    Update 5-full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,9 @@
 -- file: 5-full_table.sql
 -- Description: Below script prints the full description of the table first_table from the database hbtn_0c_0 in my MySQL server.
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, COLUMN_DEFAULT, IS_NULLABLE
+SELECT CONCAT('`', COLUMN_NAME, '`', ' ', COLUMN_TYPE, IF(IS_NULLABLE = 'NO', ' NOT NULL', ''), IF(COLUMN_DEFAULT IS NOT NULL, CONCAT(' DEFAULT ', COLUMN_DEFAULT), '')) AS 'CREATE TABLE',
+    IF(COLUMN_KEY = 'PRI', 'PRIMARY KEY', '') AS 'PRIMARY KEY',
+    IF(EXTRA = 'auto_increment', 'AUTO_INCREMENT', '') AS 'AUTO_INCREMENT',
+    IF(EXTRA = 'on update CURRENT_TIMESTAMP', 'ON UPDATE CURRENT_TIMESTAMP', '') AS 'ON UPDATE'
 FROM information_schema.columns
 WHERE table_name = 'first_table' AND table_schema = 'hbtn_0c_0';
+
