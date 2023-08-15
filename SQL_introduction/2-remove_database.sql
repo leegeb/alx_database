63e4caccfe1776c0d891e6761dbdd18724226907
lydia gebre <lydiakgebre@gmail.com>
2023-08-15 05:04:04 +0400

    Removes the database

---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- File: delete_database_if_exists.sql
+-- Description: This script deletes the database hbtn_0c_0 if it exists
+
+-- Try to delete the database hbtn_0c_0 if it exists.
+DROP DATABASE IF EXISTS hbtn_0c_0;
