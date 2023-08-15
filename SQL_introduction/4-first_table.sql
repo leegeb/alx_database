8e2880177d3b9622235d27da56ce331dba8443ce
lydia gebre <lydiakgebre@gmail.com>
2023-08-15 05:36:47 +0400

    Create table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,6 @@
 -- file: 4-first_table.sql
 -- Description: Below script creates a table called first_table in the current database in my MySQL server
 
-mysql -h localhost -u root -p -N -e "USE hbtn_0c_0; SHOW TABLES;"
+mysql -h localhost -u root -p -e "USE your_database_name; CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));"
+
+
