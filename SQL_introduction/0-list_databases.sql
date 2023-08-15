e6fe50fdc0383ee69eaaa2be338928e236e8638c
lydia gebre <lydiakgebre@gmail.com>
2023-08-15 04:34:53 +0400

    Lists -My-SQL

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,1 +1,6 @@
+/*File: 0-list_databases.sql
+Description: The SQL script lists all the databases from my server*/
+
+/*This script lists all databases of my MySQL server*/
 SHOW DATABASES;
+
---
+++ SQL_introduction/README.md
@@ -0,0 +1,5 @@
+# SQL Database Project
+
+SQL - Introduction
+
+This project contains SQL queries for creating, updating, and querying user data in a MySQL database.
