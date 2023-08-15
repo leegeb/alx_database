ce59c5e7866db739c69d36d11df5949b320f772e
lydia gebre <lydiakgebre@gmail.com>
2023-08-15 05:41:26 +0400

    Update 4-first_table.sql

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,7 +1,8 @@
 -- file: 4-first_table.sql
 -- Description: Below script creates a table called first_table in the current database in my MySQL server
 
-mysql -h localhost -u root -p -e "USE hbtn_0c_0; CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256)); INSERT INTO first_table (id, name) VALUES (1, 'John'), (2, 'Jane');"
+read -s -p "Enter password: " mysql_password && mysql -h localhost -u root -p$mysql_password -N -e "USE hbtn_0c_0; SELECT 'Tables_in_hbtn_0c_0'; first_table"
+
 
 
 
