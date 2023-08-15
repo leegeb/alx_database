1a669e6ff8c919cd92da481cc716157d2f7177c4
lydia gebre <lydiakgebre@gmail.com>
2023-08-15 05:09:05 +0400

    Lists-tables

---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,5 @@
+-- File: 3-list_tables.sql
+-- Description: script that lists all the tables of a database in my MySQL server.
+
+-- The database name will be passed as argument of mysql command
+mysql -h localhost -u root -p -e "USE root; SHOW TABLES;"
