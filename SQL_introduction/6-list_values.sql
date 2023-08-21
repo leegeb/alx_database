113686b51ac0955929d2ddca534ead4219121977
Lee <111172410+leegeb@users.noreply.github.com>
2023-08-21 16:50:06 +0400

    Update 6-list_values.sql

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,5 +1,4 @@
 -- file: 6-list_values.sql
 -- Description: Below script that lists all rows of the table first_table from the database hbtn_0c_0 in my MySQL server.
-USE mysql;
 SELECT * FROM first_table;
 
