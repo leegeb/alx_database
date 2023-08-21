f5bb39e40b925ffe3e53fc589dd63154d0e53226
Lee <111172410+leegeb@users.noreply.github.com>
2023-08-21 17:07:12 +0400

    Create 8-count_89.sql

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- file: 8-count_89.sql
+-- Description: script that creates a table second_table in the database hbtn_0c_0 in my MySQL server and add multiples rows.
+SELECT COUNT(id) FROM first_table WHERE id = 89;
