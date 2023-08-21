ac10c06ec61a3a0111be4c7592ad4e0e36fef9ab
Lee <111172410+leegeb@users.noreply.github.com>
2023-08-21 16:56:30 +0400

    Update 5-full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- file: 5-full_table.sql
 -- Description: Below script prints the full description of the table first_table from the database hbtn_0c_0 in my MySQL server.
-SHOW CREATE TABLE 'first_table';
+SHOW CREATE TABLE `first_table`;
